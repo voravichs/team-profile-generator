d353bbdd079863f9d05c3a4ce251d6fcb518c0f0
VoravichS <vsilapac@gmail.com>
2022-08-13 15:40:28 -0500

    classes definition

---
+++ index.js
@@ -0,0 +1,7 @@
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+
+const example = new Engineer('Steve', 12, 'example@example', 'Brown');
+console.log(example.getRole());
